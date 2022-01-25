532b6397945804379aec5c9a2f2518f00afa91e7
Rolando Flores <rolo@tutamail.com>
2022-01-24 21:15:17 -0600

    quité elm-module-tailwing para simple tailwind y arreglé nav font

--- src/Shared.elm
+++ src/Shared.elm
@@ -8,16 +8,13 @@ import Html as Html exposing (Html, div, text)
 import Html.Attributes as Attr exposing (class)
 import Html.Attributes.Aria as Aria
 import Html.Events as Event
-import Html.Styled as Htmls
-import Html.Styled.Attributes as Attrs exposing (css)
-import Html.Styled.Events as Events
 import Pages.Flags
 import Pages.PageUrl exposing (PageUrl)
 import Path exposing (Path)
 import Route exposing (Route)
 import SharedTemplate exposing (SharedTemplate)
-import Svg.Styled exposing (path, svg)
-import Svg.Styled.Attributes as SvgAttr
+import Svg exposing (path, svg)
+import Svg.Attributes as SvgAttr
 import Tailwind.Breakpoints as TwBp
 import Tailwind.Utilities as Tw
 import View exposing (View)
@@ -122,7 +119,6 @@ view sharedData page model toMsg pageView =
         div
             []
             ((myNav model pageView.menu
-                |> Htmls.toUnstyled
                 |> Html.map toMsg
              )
                 :: div
@@ -164,116 +160,85 @@ indexViewFooter =
 myNav :
     Model
     -> List View.Liga
-    -> Htmls.Html Msg
+    -> Html Msg
 myNav modelo getMenu =
     let
-        myLogoAndLinks : Htmls.Html Msg
+        myLogoAndLinks : Html Msg
         myLogoAndLinks =
-            Htmls.div
-                [ css [ Tw.flex, Tw.items_center ] ]
-                [ Htmls.div
+            div
+                [ class "flex items-center" ]
+                [ div
                     -- EL LOGO
-                    [ css [ Tw.flex_shrink_0 ] ]
-                    [ Htmls.img
-                        [ css [ Tw.h_8, Tw.w_8 ]
-                        , Attrs.src "https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
-                        , Attrs.alt "Workflow"
+                    [ class "flex-shrink-0" ]
+                    [ Html.img
+                        [ class "h-8 w-8"
+                        , Attr.src "https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
+                        , Attr.alt "Workflow"
                         ]
                         []
                     ]
-                , Htmls.div
+                , div
                     -- LIGAS DE NAVEGACION
-                    [ css [ Tw.hidden, TwBp.md [ Tw.block ] ] ]
-                    [ Htmls.div
-                        [ css
-                            [ Tw.ml_10
-                            , Tw.flex
-                            , Tw.items_baseline
-                            , Tw.space_x_4
-                            ]
-                        ]
-                        (ligasChulas False <| getMenu)
+                    [ class "hidden, md:block" ]
+                    [ div
+                        [ class "ml-10 flex items-baseline space-x-4" ]
+                        (ligasChulas False getMenu)
                     ]
                 ]
 
-        myHiddenMenu : Htmls.Html Msg
+        myHiddenMenu : Html Msg
         myHiddenMenu =
-            Htmls.div
-                [ css <|
-                    TwBp.md [ Tw.hidden ]
-                        :: (if modelo.showMobileMenu then
-                                [ Tw.block ]
+            div
+                [ class <|
+                    "md:hidden "
+                        ++ (if modelo.showMobileMenu then
+                                "block"
 
                             else
-                                [ Tw.hidden ]
+                                "hidden"
                            )
                 ]
-                [ Htmls.div
-                    [ css
-                        [ Tw.px_2
-                        , Tw.pt_2
-                        , Tw.pb_3
-                        , Tw.space_y_1
-                        , TwBp.sm [ Tw.px_3 ]
-                        , Tw.flex
-                        , Tw.flex_col
-                        ]
-                    ]
-                    (ligasChulas True <| getMenu)
+                [ div
+                    [ class "px-2 pt-2 pb-3 space-y-1 sm:px-3 sm:flex sm:flex-col" ]
+                    (ligasChulas True getMenu)
                 ]
 
-        ligasChulas :
-            Bool
-            -> List View.Liga
-            -> List (Htmls.Html Msg)
-        ligasChulas esMovil menus =
+        ligasChulas : Bool -> List View.Liga -> List (Html Msg)
+        ligasChulas showMobileLinks menus =
             let
-                clasesBase =
-                    "text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md font-medium"
+                clasesQueAplican : Bool -> Bool -> Html.Attribute Msg
+                clasesQueAplican muestraMovil esResaltado =
+                    case ( muestraMovil, esResaltado ) of
+                        ( True, True ) ->
+                            class "bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium"
 
-                claseActual =
-                    "bg-gray-900 text-white px-3 py-2 rounded-md font-medium"
+                        ( True, False ) ->
+                            class "text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
 
-                claseActualEnMovil =
-                    "block text-base"
+                        ( False, True ) ->
+                            class "bg-gray-900 text-white px-3 py-2 rounded-md text-base font-medium"
 
-                claseActualEnDesktop =
-                    "text-sm"
+                        ( False, False ) ->
+                            class "text-amber-200 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-base font-medium"
 
-                claseExtraPaMovil =
-                    "block text-base"
-
-                claseExtraPaDesktop =
-                    "text-sm"
-
-                clasesQueVan esParaMovil liga =
-                    if esParaMovil then
-                        claseActual ++ " " ++ claseActualEnMovil
-
-                    else
-                        claseActual ++ " " ++ claseActualEnDesktop
-
-                ligaChula clases liga =
+                ligaChula clase liga =
                     Route.link
                         liga.direccion
-                        [ class clases ]
-                        [ Html.text liga.queDice ]
-                        |> Htmls.fromUnstyled
+                        [ clase ]
+                        [ text liga.queDice ]
             in
             List.map
-                (\algoDelMenu ->
+                (\unaLiga ->
                     ligaChula
-                        (clasesQueVan esMovil algoDelMenu)
-                        algoDelMenu
+                        (clasesQueAplican showMobileLinks False)
+                        unaLiga
                 )
                 menus
 
-        heroiconOutlineMenu : Htmls.Html Msg
+        heroiconOutlineMenu : Html.Html Msg
         heroiconOutlineMenu =
-            Htmls.div
-                [ css
-                    [ Tw.h_6, Tw.w_6, Tw.block ]
-                ]
+            div
+                [ class "h-6 w-6 block" ]
                 [ svg
                     [ SvgAttr.fill "none"
                     , SvgAttr.viewBox "0 0 24 24"
@@ -291,10 +256,10 @@ myNav modelo getMenu =
                     ]
                 ]
 
-        heroiconOutlineX : Htmls.Html Msg
+        heroiconOutlineX : Html Msg
         heroiconOutlineX =
-            Htmls.div
-                [ css [ Tw.h_6, Tw.w_6, Tw.block ] ]
+            div
+                [ class "h-6 w-6 block" ]
                 [ svg
                     [ SvgAttr.fill "none"
                     , SvgAttr.viewBox "0 0 24 24"
@@ -312,42 +277,18 @@ myNav modelo getMenu =
                     ]
                 ]
 
-        mobileMenuButton : Htmls.Html Msg
+        mobileMenuButton : Html Msg
         mobileMenuButton =
-            Htmls.div
-                [ css
-                    [ Tw.neg_mr_2
-                    , Tw.flex
-                    , TwBp.md [ Tw.hidden ]
+            div
+                [ class "-mr-2 flex md:hidden" ]
+                [ Html.button
+                    [ class "bg-gray-800 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+                    , Event.onClick ToggleMobileMenu
                     ]
-                ]
-                [ Htmls.button
-                    [ css
-                        [ Tw.bg_gray_800
-                        , Tw.inline_flex
-                        , Tw.items_center
-                        , Tw.justify_center
-                        , Tw.p_2
-                        , Tw.rounded_md
-                        , Tw.text_gray_400
-                        , Css.hover
-                            [ Tw.text_white
-                            , Tw.bg_gray_700
-                            ]
-                        , Css.focus
-                            [ Tw.outline_none
-                            , Tw.ring_2
-                            , Tw.ring_offset_2
-                            , Tw.ring_offset_gray_800
-                            , Tw.ring_white
-                            ]
-                        ]
-                    , Events.onClick ToggleMobileMenu
-                    ]
-                    [ Htmls.span
-                        [ css [ Tw.sr_only ] ]
-                        [ Htmls.text "Open main menu" ]
-                    , Htmls.span
+                    [ Html.span
+                        [ class "sr-only" ]
+                        [ text "Open main menu" ]
+                    , Html.span
                         []
                         [ if modelo.showMobileMenu then
                             heroiconOutlineX
@@ -358,25 +299,12 @@ myNav modelo getMenu =
                     ]
                 ]
     in
-    Htmls.nav
-        [ css [ Tw.bg_gray_800 ] ]
-        [ Htmls.div
-            [ css
-                [ Tw.max_w_7xl
-                , Tw.mx_auto
-                , Tw.px_4
-                , TwBp.lg [ Tw.px_8 ]
-                , TwBp.sm [ Tw.px_6 ]
-                ]
-            ]
-            [ Htmls.div
-                [ css
-                    [ Tw.flex
-                    , Tw.items_center
-                    , Tw.justify_between
-                    , Tw.h_16
-                    ]
-                ]
+    Html.nav
+        [ class "bg-gray-800" ]
+        [ div
+            [ class "max-w-7xl mx-auto px-4 lg:px-8 sm:px-6" ]
+            [ div
+                [ class "flex items-center justify-between h-16" ]
                 [ myLogoAndLinks
                 , mobileMenuButton
                 ]
